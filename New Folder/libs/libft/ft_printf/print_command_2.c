c5920a460be50b7d5abad47b9b6c0fa91553237e
Mahdi42x <146098014+Mahdi42x@users.noreply.github.com>
2025-02-03 15:52:01 +0100

    Add files via upload

---
+++ New Folder/libs/libft/ft_printf/print_command_2.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_command_2.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emkalkan <emkalkan@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/26 13:08:46 by mawada            #+#    #+#             */
+/*   Updated: 2025/01/19 13:35:16 by emkalkan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int print_ptr(unsigned long long nbr)
+{
+    int count;
+    char *symbols;
+
+    symbols = "0123456789abcdef";
+    if (nbr < 16)
+        return (print_char(symbols[nbr]));
+    else
+    {
+        count = print_ptr(nbr / 16);
+        return (count + print_char(symbols[nbr % 16]));
+    }
+}
+
+
+int	print_pointer(va_list ap)
+{
+	unsigned long long int	num;
+	int						count;
+
+	count = 0;
+	num = va_arg(ap, unsigned long long int);
+	if (num == 0)
+		count += print_str("(nil)");
+	else
+	{
+		count += print_str("0x");
+		count += print_ptr(num);
+	}
+	return (count);
+}
