226c4d665f36f56ba57f0d94cc6c7cf1acd20473
Mahdi42x <146098014+Mahdi42x@users.noreply.github.com>
2025-02-24 16:55:08 +0100

    libft

--- libs/libft/ft_gnl/get_next_line.c
+++ libs/libft/ft_gnl/get_next_line.c
@@ -89,3 +89,96 @@ static char	*fill_linesbuffer(int fd, char *rest, char *buffer)
 	}
 	return (rest);
 }
+
+// char	*get_next_line(int fd)
+// {
+// 	static char	*rest;
+// 	char		*line;
+// 	char		*buffer;
+
+// 	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+// 	if (!buffer)
+// 		return (NULL);
+// 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+// 	{
+// 		free(rest);    // Freigabe des alten rest-Speichers
+// 		free(buffer);
+// 		rest = NULL;
+// 		buffer = NULL;
+// 		return (NULL);
+// 	}
+// 	line = fill_linesbuffer(fd, rest, buffer);
+// 	free(buffer);
+// 	buffer = NULL;
+// 	if (!line)
+// 		return (NULL);
+
+// 	// Freigabe des alten rest-Speichers, bevor er überschrieben wird
+// 	if (rest)
+// 	{
+// 		free(rest);
+// 		rest = NULL;
+// 	}
+
+// 	rest = fixline(line);
+// 	return (line);
+// }
+
+// static char	*fill_linesbuffer(int fd, char *rest, char *buffer)
+// {
+// 	ssize_t	readstatus;
+// 	char	*tmp;
+
+// 	readstatus = 1;
+// 	while (readstatus > 0)
+// 	{
+// 		readstatus = read(fd, buffer, BUFFER_SIZE);
+// 		if (readstatus == -1)
+// 			return (NULL);
+// 		else if (readstatus == 0)
+// 			break ;
+// 		buffer[readstatus] = '\0';
+// 		if (!rest)
+// 		{
+// 			rest = (char *)malloc(1);
+// 			rest[0] = '\0';
+// 		}
+// 		tmp = rest;
+// 		rest = ft_strjoin(tmp, buffer);
+// 		free(tmp);
+// 		tmp = NULL;
+// 		if (ft_strchr(buffer, '\n'))
+// 			break ;
+// 	}
+
+// 	// Freigabe von rest, wenn das Ende der Datei erreicht ist
+// 	if (readstatus == 0 && rest && *rest == '\0')
+// 	{
+// 		free(rest);
+// 		rest = NULL;
+// 	}
+
+// 	return (rest);
+// }
+
+// static char	*fixline(char *line_buffer)
+// {
+// 	char	*rest;
+// 	ssize_t	i;
+
+// 	i = 0;
+// 	while (line_buffer[i] != '\n' && line_buffer[i] != '\0')
+// 		i++;
+// 	if (line_buffer[i] == 0)
+// 		return (NULL);
+// 	rest = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - i);
+// 	if (!rest)
+// 		return (NULL);  // Fehlerbehandlung, falls ft_substr fehlschlägt
+// 	if (*rest == 0)
+// 	{
+// 		free(rest);
+// 		rest = NULL;
+// 	}
+// 	line_buffer[i + 1] = 0;
+// 	return (rest);
+// }
--- libs/libft/ft_printf/print_command_2.c
+++ libs/libft/ft_printf/print_command_2.c
@@ -3,30 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   print_command_2.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emkalkan <emkalkan@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: mawada <mawada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 13:08:46 by mawada            #+#    #+#             */
-/*   Updated: 2025/01/19 13:35:16 by emkalkan         ###   ########.fr       */
+/*   Updated: 2025/02/24 16:50:46 by mawada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int print_ptr(unsigned long long nbr)
+int	print_ptr(unsigned long long nbr)
 {
-    int count;
-    char *symbols;
+	int		count;
+	char	*symbols;
 
-    symbols = "0123456789abcdef";
-    if (nbr < 16)
-        return (print_char(symbols[nbr]));
-    else
-    {
-        count = print_ptr(nbr / 16);
-        return (count + print_char(symbols[nbr % 16]));
-    }
+	symbols = "0123456789abcdef";
+	if (nbr < 16)
+		return (print_char(symbols[nbr]));
+	else
+	{
+		count = print_ptr(nbr / 16);
+		return (count + print_char(symbols[nbr % 16]));
+	}
 }
-
 
 int	print_pointer(va_list ap)
 {
